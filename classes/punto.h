7759f4e1dcbb398dbbed2ce481265b6c063d49b8
Ignacio Tampe <thenachotp@gmail.com>
2018-11-29 01:51:34 -0300

    Borrado de elementos antiguos

--- classes/punto.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-class Punto {
-    public:
-        float x, y;
-        Punto::Punto(float newX, float newY);
-        std::ostream& operator<< (std::ostream &out, Punto const& pto);
-
-}
